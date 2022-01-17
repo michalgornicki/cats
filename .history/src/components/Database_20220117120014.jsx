d7478e4cc03639c0cc8d8dd5609ee448972fb288
Michal Gornicki <gornickimichal@gmail.com>
2022-01-17 16:28:23 +0100

    fix login

---
+++ .history/src/components/Database_20220117120014.jsx
@@ -0,0 +1,71 @@
+import Container from "react-bootstrap/Container";
+import Card from "react-bootstrap/Card";
+import catPhoto from "../cat1.png";
+import catPhoto2 from "../photos/1.jpg";
+import catPhoto3 from "../photos/2.jpg";
+import catPhoto4 from "../photos/3.jpg";
+
+const Database = () => {
+  return (
+    <div>
+      <Container>
+        <div className="display-6">
+          Znaleźliśmy ... kotów w Twojej lokalizacji
+        </div>
+        <Container className="w-75 my-4 mx-auto d-flex flex-wrap">
+          <Card className="card">
+            <Card.Img className="card-photo" variant="top" src={catPhoto2} />
+            <Card.Body>
+              <Card.Title>Minerwa, kotka</Card.Title>
+              <Card.Text>Kraków, Małopolskie</Card.Text>
+            </Card.Body>
+          </Card>
+
+        </Container>
+      </Container>
+
+      <Container className="checkboxes">
+        <form className="d-flex flex-column m-3">
+            <div className="div">Lokalizacja</div>
+          <div><input type="checkbox" id="vehicle1" name="vehicle1" value="Bike" />
+          <label for="vehicle1">Mazowieckie</label>
+          </div>
+          <div><input type="checkbox" id="vehicle1" name="vehicle1" value="Bike" />
+          <label for="vehicle1">Podlaskie</label>
+          </div>
+          <div><input type="checkbox" id="vehicle1" name="vehicle1" value="Bike" />
+          <label for="vehicle1">Lubelskie</label>
+          </div>
+        </form>
+
+        <form className="d-flex flex-column m-3">
+            <div className="div">Wiek</div>
+          <div><input type="checkbox" id="vehicle1" name="vehicle1" value="Bike" />
+          <label for="vehicle1">0-2 lata</label>
+          </div>
+          <div><input type="checkbox" id="vehicle1" name="vehicle1" value="Bike" />
+          <label for="vehicle1">3-5 lat</label>
+          </div>
+          <div><input type="checkbox" id="vehicle1" name="vehicle1" value="Bike" />
+          <label for="vehicle1">6-10 lat</label>
+          </div>
+          <div><input type="checkbox" id="vehicle1" name="vehicle1" value="Bike" />
+          <label for="vehicle1">11+ lat</label>
+          </div>
+        </form>
+
+        <form className="d-flex flex-column m-3">
+            <div className="div">Płeć</div>
+          <div><input type="checkbox" id="vehicle1" name="vehicle1" value="Bike" />
+          <label for="vehicle1">Kot</label>
+          </div>
+          <div><input type="checkbox" id="vehicle1" name="vehicle1" value="Bike" />
+          <label for="vehicle1">Kotka</label>
+          </div>
+        </form>
+      </Container>
+    </div>
+  );
+};
+
+export default Database;
---
+++ .history/src/components/Login_20220117111350.jsx
@@ -0,0 +1,32 @@
+import Button from "react-bootstrap/Button";
+import { useAuth0 } from "@auth0/auth0-react";
+
+const Login = () => {
+  const { loginWithRedirect } = useAuth0();
+
+  const { logout } = useAuth0();
+  const { user, isAuthenticated, isLoading } = useAuth0();
+
+  return (
+    <div>
+      {!isAuthenticated ? (
+        <Button onClick={() => loginWithRedirect()} className=" m-1" variant="success">
+          Zaloguj się
+        </Button>
+      ) : (
+        <div>
+
+          <img className="" src={user.picture} alt={user.name} />
+          <Button onClick={""} className=" m-1" variant="success">
+            Moje konto
+          </Button>
+          <Button onClick={() => logout({ returnTo: window.location.origin })} className=" m-1" variant="success">
+            Wyloguj
+          </Button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Login;
