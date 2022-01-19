adbface7b1a9f025f2c7bbacc88d6d223163cc99
Michal Gornicki <gornickimichal@gmail.com>
2022-01-19 13:39:32 +0100

    add hidden data

---
+++ .history/src/components/profiles/1_20220119133119.jsx
@@ -0,0 +1,58 @@
+import Container from "react-bootstrap/Container";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import data from "../../data";
+import { useAuth0 } from "@auth0/auth0-react";
+
+const Cat1 = () => {
+
+  const { user, isAuthenticated, isLoading } = useAuth0();
+
+
+  const Copy = () => {
+    window.navigator.clipboard.writeText("https://michalgornicki.github.io/cats/#/profiles/" + data[0].id);
+  }
+
+  return (
+    <Container className="fading">
+      <div className="display-6">Profil kota</div>
+
+      <Card className="card-profile">
+        <Card.Title className="display-6 mb-5">Cześć, nazywam się {data[0].name}!</Card.Title>
+        <div className="d-flex flex-row align-bottom">
+          <Card.Img className="card-profile-photo" variant="top" src={data[0].image} />
+          <div className="buttons w-50 m-4">
+          <Button className="m-1 w-50" variant="success">
+              Telefon: {isAuthenticated? data[0].phone : "***-***-***"}
+            </Button>
+            <Button className="m-1 w-50" variant="success">
+            {isAuthenticated? "Dodaj do ulubionych &#10084;" : "Zaloguj się aby dodać" + &#10084;}	
+            </Button>
+            <Button className="m-1 w-50" variant="success" onClick={Copy}>
+              Kopiuj link &#128236;
+            </Button>
+          </div>
+        </div>
+        <div className="flex w-25 m-1">
+          <Card.Img className="card-profile-photo-sm" variant="top" src={data[0].image} />
+          <Card.Img className="card-profile-photo-sm" variant="top" src={data[0].image} />
+          <Card.Img className="card-profile-photo-sm" variant="top" src={data[0].image} />
+        </div>
+        <Card.Body>
+          <div className="display-6 my-3">O mnie:</div>
+          <div className="div">Wiek: {data[0].age} lat</div>
+          <div className="div">Płeć: {data[0].sex}</div>
+          <div className="div">Kolor sierści: {data[0].color}</div>
+          <div className="display-6 my-3">Moja historia:</div>
+          <div className="div">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+            enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+            in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+          </div>
+        </Card.Body>
+      </Card>
+    </Container>
+  );
+};
+
+export default Cat1;
